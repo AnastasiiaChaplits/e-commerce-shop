413669052aa0c8d785daed38c8aa4b21deea1704
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-05-13 17:01:53 +0300

    memoized selectors with reselect

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,19 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./rootReducer";
 
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+//   console.log("type", action.type);
+//   console.log("payload", action.payload);
+//   console.log("currentState", store.getState());
+//
+//   next(action);
+//
+//   console.log("next state", store.getState());
+// };
+
 const middlewares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
