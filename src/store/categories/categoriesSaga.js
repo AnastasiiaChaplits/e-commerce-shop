36359a8b6a9026813ee01671f368abe017b4365b
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-05-16 13:50:28 +0300

    refactored with redux-saga

---
+++ src/store/categories/categoriesSaga.js
@@ -0,0 +1,28 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
+import {
+  fetchCategoriesFailed,
+  fetchCategoriesSuccess,
+} from "./categoriesAction";
+import { CategoriesActionTypes } from "./categoriesTypes";
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
+    yield put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield put(fetchCategoriesFailed(error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield takeLatest(
+    CategoriesActionTypes.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield all([call(onFetchCategories)]);
+}
